d9538c502d09f2ecacf41c04674d713abf89cdef
Sumir-14 <165183364+Sumir-14@users.noreply.github.com>
2024-04-04 16:50:37 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@conditionals/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //  int score;
+  // cout << "Enter the score - " << endl;
+  // cin >> score;
+  // if(score < 300){
+  //   cout << "India wins" << endl;
+  // }
+  // else if (score >300){
+  //   cout <<"Pakistan wins" << endl;
+
+  // }
+
+//   int marks;
+//   cout << "Enter the marks - " << endl;
+//   cin >> marks;
+
+//   if (marks >= 90) {
+//     cout << "You have got A grade" << endl;
+
+//   } else if (marks >= 80) {
+//     cout << "You have got B grade" << endl;
+
+    //   } else if (marks >= 70) {
+     //     cout << "You have got C grade" << endl;
+
+   //   } else if (marks >= 60) {
+     //     cout << "You have got D grade" << endl;
+
+  //   } else if (marks >= 50) {
+  //     cout << "You have got E grade" << endl;
+
+  //   } else if (marks < 50) {
+  //     cout << "You have got F grade" << endl;
+  //   }
+  // for(int i=0; i<5; i=i+1)
+  //   cout << "Sumir" << endl;
+
+
+  // for (int i=1 ; i<=10; i= i+1)
+  //  cout << 2*i << endl;
+
+
+  int n;
+  if( cin >> n){
+    cout << "Sumir" << endl;
+  }
+}
